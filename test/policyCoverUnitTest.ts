ed3c7c90e8ae383536f51c8638e9c1e235274ead
Tien Thao NGUYEN <ntienthao87@gmail.com>
2022-05-25 10:43:06 +0200

    fixe merge after imp withdrawPolicy

--- test/policyCoverUnitTest.ts
+++ test/policyCoverUnitTest.ts
@@ -45,7 +45,7 @@ describe("Policy cover contract", function () {
     ).to.not.equal("0x");
   });
 
-  it.skip("Should initialize slot0 in constructor", async function () {
+  it("Should initialize slot0 in constructor", async function () {
     const slot0 = await POLICY_COVER_CONTRACT_TEST.getSlot0();
 
     expect(slot0.tick).to.be.equal(BN(0));
@@ -73,8 +73,8 @@ describe("Policy cover contract", function () {
     return array;
   }
 
-  it.skip("Should buy policy", async () => {
-    for (let i = 0; i < 3; i++) {
+  it("Should buy policy", async () => {
+    for (let i = 0; i < 2; i++) {
       // await hre.network.provider.request({
       //   method: "hardhat_reset",
       //   params: [
@@ -337,71 +337,5 @@ describe("Policy cover contract", function () {
 
     const totalInsured2 = await POLICY_COVER_CONTRACT_TEST.getTotalInsured();
     expect(totalInsured2).to.be.equal(BN(0));
-  });
-
-  it("buy pilicy", async () => {
-    for (let i = 0; i < 3; i++) {
-      // await hre.network.provider.request({
-      //   method: "hardhat_reset",
-      //   params: [
-      //     {
-      //       forking: {
-      //         // jsonRpcUrl: process.env.MAINNET_URL,
-      //         blockNumber: 14307200,
-      //       },
-      //     },
-      //   ],
-      // });
-
-      await POLICY_COVER_CONTRACT_TEST.setTick(0);
-      await POLICY_COVER_CONTRACT_TEST.setRate(1);
-      await POLICY_COVER_CONTRACT_TEST.setEmissionRate(0);
-      await POLICY_COVER_CONTRACT_TEST.setHoursPerTick(48);
-      await POLICY_COVER_CONTRACT_TEST.setPremiumSpent(0);
-      // await POLICY_COVER_CONTRACT_TEST.setLastUpdateTimestamp(1646219106);
-
-      await increaseTimeAndMine(10 * 24 * 60 * 60);
-
-      await POLICY_COVER_CONTRACT_TEST.testBuyPolicy(7300, 365000);
-
-      let slot0 = await POLICY_COVER_CONTRACT_TEST.getSlot0();
-
-      expect(slot0.tick).to.be.equal(5);
-      expect(slot0.useRate).to.be.equal(2);
-      expect(slot0.emissionRate).to.be.equal(20);
-      expect(slot0.hoursPerTick).to.be.equal(24);
-      expect(slot0.premiumSpent).to.be.equal(0);
-
-      await increaseTimeAndMine(10 * 24 * 60 * 60);
-
-      const resultB = await POLICY_COVER_CONTRACT_TEST.testBuyPolicy(
-        14600,
-        365000
-      );
-
-      slot0 = await POLICY_COVER_CONTRACT_TEST.getSlot0();
-
-      expect(slot0.tick).to.be.equal(15);
-      expect(slot0.useRate).to.be.equal(4);
-      expect(slot0.emissionRate).to.be.equal(80);
-      expect(slot0.hoursPerTick).to.be.equal(12);
-      expect(slot0.premiumSpent).to.be.equal(200);
-
-      await increaseTimeAndMine(1000 * 24 * 60 * 60);
-
-      console.log("Final actualizing");
-      const array3 = await actualizing();
-      console.log(`array 3: ${array3}`);
-      expect(array3.length).to.be.equal(2);
-      expect(array3[0].nbrDays.toString()).to.be.equal("177");
-      expect(array3[1].nbrDays.toString()).to.be.equal("375");
-
-      slot0 = await POLICY_COVER_CONTRACT_TEST.getSlot0();
-      // expect(slot0.tick).to.be.equal(15);
-      expect(slot0.useRate).to.be.equal(1);
-      expect(slot0.emissionRate).to.be.equal(0);
-      expect(slot0.hoursPerTick).to.be.equal(48);
-      expect(slot0.premiumSpent).to.be.equal(21900);
-    }
   });
 });
