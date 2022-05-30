28e5c666b1c3275e6e86315c99fc839c7fd96bd8
Tien Thao NGUYEN <ntienthao87@gmail.com>
2022-05-30 05:53:06 +0200

    remove Wad

--- contracts/libraries/WadRayMath.sol
+++ contracts/libraries/WadRayMath.sol
@@ -2,19 +2,14 @@
 pragma solidity ^0.8;
 
 /**
- * @title WadRayMath library
+ * @title RayMath library
  * @author Aave
  * @dev Provides mul and div function for wads (decimal numbers with 18 digits precision) and rays (decimals with 27 digits)
  **/
 
 library WadRayMath {
-  uint256 internal constant WAD = 1e18;
-  uint256 internal constant halfWAD = WAD / 2;
-
   uint256 internal constant RAY = 1e27;
   uint256 internal constant halfRAY = RAY / 2;
-
-  uint256 internal constant WAD_RAY_RATIO = 1e9;
 
   /**
    * @return One ray, 1e27
@@ -24,56 +19,10 @@ library WadRayMath {
   }
 
   /**
-   * @return One wad, 1e18
-   **/
-
-  function wad() internal pure returns (uint256) {
-    return WAD;
-  }
-
-  /**
    * @return Half ray, 1e27/2
    **/
   function halfRay() internal pure returns (uint256) {
     return halfRAY;
-  }
-
-  /**
-   * @return Half ray, 1e18/2
-   **/
-  function halfWad() internal pure returns (uint256) {
-    return halfWAD;
-  }
-
-  /**
-   * @dev Multiplies two wad, rounding half up to the nearest wad
-   * @param a Wad
-   * @param b Wad
-   * @return The result of a*b, in wad
-   **/
-  function wadMul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0 || b == 0) {
-      return 0;
-    }
-
-    require(a <= (type(uint256).max - halfWAD) / b, "Overflow in wadMul");
-
-    return (a * b + halfWAD) / WAD;
-  }
-
-  /**
-   * @dev Divides two wad, rounding half up to the nearest wad
-   * @param a Wad
-   * @param b Wad
-   * @return The result of a/b, in wad
-   **/
-  function wadDiv(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b != 0, "Division by zero");
-    uint256 halfB = b / 2;
-
-    require(a <= (type(uint256).max - halfB) / WAD, "Overflow in wadDiv");
-
-    return (a * WAD + halfB) / b;
   }
 
   /**
@@ -92,6 +41,20 @@ library WadRayMath {
     return (a * b + halfRAY) / RAY;
   }
 
+  function rayMulRoundingDown(uint256 a, uint256 b)
+    internal
+    pure
+    returns (uint256)
+  {
+    if (a == 0 || b == 0) {
+      return 0;
+    }
+
+    require(a <= type(uint256).max / b, "Overflow in rayMul");
+
+    return (a * b) / RAY;
+  }
+
   /**
    * @dev Divides two ray, rounding half up to the nearest ray
    * @param a Ray
@@ -107,27 +70,15 @@ library WadRayMath {
     return (a * RAY + halfB) / b;
   }
 
-  /**
-   * @dev Casts ray down to wad
-   * @param a Ray
-   * @return a casted to wad, rounded half up to the nearest wad
-   **/
-  function rayToWad(uint256 a) internal pure returns (uint256) {
-    uint256 halfRatio = WAD_RAY_RATIO / 2;
-    uint256 result = halfRatio + a;
-    require(result >= halfRatio, "Overflow in rayToWad");
+  function rayDivRoundingDown(uint256 a, uint256 b)
+    internal
+    pure
+    returns (uint256)
+  {
+    require(b != 0, "Division by zero");
 
-    return result / WAD_RAY_RATIO;
-  }
+    require(a <= type(uint256).max / RAY, "Overflow in rayDiv");
 
-  /**
-   * @dev Converts wad up to ray
-   * @param a Wad
-   * @return a converted in ray
-   **/
-  function wadToRay(uint256 a) internal pure returns (uint256) {
-    uint256 result = a * WAD_RAY_RATIO;
-    require(result / WAD_RAY_RATIO == a, "Overflow in wadToRay");
-    return result;
+    return (a * RAY) / b;
   }
 }
