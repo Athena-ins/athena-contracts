87c8075079d6d55911018d76114403e7f7b4b482
Tien-Thao-NGUYEN <ntienthao87@gmail.com>
2022-08-18 15:05:04 +0200

    remove ray and halfRay fct in RayMath.sol

--- contracts/libraries/RayMath.sol
+++ contracts/libraries/RayMath.sol
@@ -4,26 +4,12 @@ pragma solidity ^0.8;
 /**
  * @title RayMath library
  * @author Aave
- * @dev Provides mul and div function for wads (decimal numbers with 18 digits precision) and rays (decimals with 27 digits)
+ * @dev Provides mul and div function for rays (decimals with 27 digits)
  **/
 
 library RayMath {
   uint256 internal constant RAY = 1e27;
   uint256 internal constant halfRAY = RAY / 2;
-
-  /**
-   * @return One ray, 1e27
-   **/
-  function ray() internal pure returns (uint256) {
-    return RAY;
-  }
-
-  /**
-   * @return Half ray, 1e27/2
-   **/
-  function halfRay() internal pure returns (uint256) {
-    return halfRAY;
-  }
 
   /**
    * @dev Multiplies two ray, rounding half up to the nearest ray
