2539c381ac807cc4af9635a89944f27911e8c767
VirtualBlackWhale <victor.brun@ymail.com>
2022-12-15 13:27:24 +0100

    feat(staking): add view position function

--- contracts/interfaces/IStakedAten.sol
+++ contracts/interfaces/IStakedAten.sol
@@ -5,11 +5,13 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 interface IStakedAten is IERC20 {
-    function stake(
-        address _account,
-        uint256 _amount,
-        uint256 _usdDeposit
-    ) external;
+  function stake(
+    address _account,
+    uint256 _amount,
+    uint256 _usdDeposit
+  ) external;
 
-    function withdraw(address _account, uint256 _amount) external;
+  function withdraw(address _account, uint256 _amount) external;
+
+  function positionOf(address _account) external view returns (uint256);
 }
