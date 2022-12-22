c6a92081a91dc7790a32d9163445f5ab3ae47668
Blackwhale <victor.blackwhale@gmail.com>
2022-12-22 14:24:57 +0100

    feat(claim evidence): create single function for init events + submit multi evidence + save registered evidence

--- contracts/ClaimEvidence.sol
+++ contracts/ClaimEvidence.sol
@@ -7,47 +7,70 @@ contract ClaimEvidence is IEvidence {
   IArbitrator public immutable arbitrator;
 
   // Maps a protocol ID to its generic meta-evidence IPFS file hash
-  mapping(uint256 => string) public metaEvidenceForProtocol;
+  mapping(uint256 => string) public protocolToMetaEvidence;
 
-  // Maps a claim ID to its designated meta-evidence ID
-  mapping(uint256 => uint256) public claimIdToMetaEvidenceId;
+  // Maps a dispute ID to its submited evidence
+  mapping(uint256 => string[]) public disputeToEvidence;
 
   constructor(IArbitrator arbitrator_) {
     arbitrator = arbitrator_;
   }
 
-  function _emitDisputeEvent(uint256 claimId_) internal {
+  function _genMetaEvidenceId(uint256 disputeId_, uint256 protocolId_)
+    internal
+    pure
+    returns (uint256)
+  {
+    return protocolId_ * 1e10 + disputeId_;
+  }
+
+  function _emitKlerosDisputeEvents(uint256 disputeId_, uint256 protocolId_)
+    internal
+  {
+    // @bw should add initial kleros creation event from arbitrator
+
+    uint256 metaEvidenceId = _genMetaEvidenceId(disputeId_, protocolId_);
+
     emit Dispute(
-      arbitrator, // IArbitrator indexed _arbitrator,
-      claimId_, // uint256 indexed _disputeID
-      111111111111, // uint256 _metaEvidenceID
-      111111111111 // uint256 _evidenceGroupID
+      arbitrator, // IArbitrator indexed _arbitrator
+      disputeId_, // uint256 indexed _disputeID
+      metaEvidenceId, // uint256 _metaEvidenceID
+      disputeId_ // uint256 _evidenceGroupID
     );
+
+    // Get the address of the meta-evidence dedicated to the protocol
+    string memory protocolMetaEvidence = protocolToMetaEvidence[protocolId_];
+
+    // Emit the meta-evidence event
+    emit MetaEvidence(
+      metaEvidenceId, // uint256 indexed _metaEvidenceID
+      protocolMetaEvidence // string _evidence
+    );
+  }
+
+  function _submitKlerosEvidence(
+    uint256 disputeId_,
+    address party_,
+    string[] calldata ipfsEvidenceHashes_
+  ) internal {
+    for (uint256 i = 0; i < ipfsEvidenceHashes_.length; i++) {
+      // Save evidence files
+      disputeToEvidence[disputeId_].push(ipfsEvidenceHashes_[i]);
+
+      // Emit event for Kleros to pick up the evidence
+      emit Evidence(
+        arbitrator, // IArbitrator indexed _arbitrator
+        disputeId_, // uint256 indexed _evidenceGroupID
+        party_, // address indexed _party
+        ipfsEvidenceHashes_[i] // string _evidence
+      );
+    }
   }
 
   function _addMetaEvidenceForProtocol(
     uint256 protocolId_,
     string calldata metaEvidenceIpfsHash_
   ) internal {
-    metaEvidenceForProtocol[protocolId_] = metaEvidenceIpfsHash_;
-  }
-
-  function _submitMetaEvidenceForClaim(
-    uint256 protocolId_,
-    string calldata evidence_
-  ) internal {
-    emit MetaEvidence(
-      111111111111, // uint256 indexed _metaEvidenceID
-      evidence_ // string _evidence
-    );
-  }
-
-  function _submitEvidenceForClaim(uint256 claimId_) internal {
-    emit Evidence(
-      arbitrator, // IArbitrator indexed _arbitrator
-      111111111111, // uint256 indexed _evidenceGroupID
-      address(0), // address indexed _party
-      "" // string _evidence
-    );
+    protocolToMetaEvidence[protocolId_] = metaEvidenceIpfsHash_;
   }
 }
