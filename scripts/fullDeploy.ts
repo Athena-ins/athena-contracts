01841ca6e804f88669489b2d8cf22e898d8d14e3
VirtualBlackWhale <victor.brun@ymail.com>
2023-01-13 17:30:05 +0100

    fix(deployer script): update addresses + use current deployment addresses + add logs + uniform naming with helper & frontend

--- scripts/fullDeploy.ts
+++ scripts/fullDeploy.ts
@@ -1,5 +1,5 @@
-import { ethers as hre_ethers } from "hardhat";
-import ProtocolHelper from "../test/helpers/ProtocolHelper";
+import hre, { ethers as hre_ethers } from "hardhat";
+import ProtocolHelper, { CONTRACT } from "../test/helpers/ProtocolHelper";
 //
 import abiERC20 from "../abis/weth.json";
 
@@ -10,19 +10,19 @@ const ADDRESS = {
   deployer: "0x745A6BE3C44883979FC43e1Df2F7e83eE7b9f73A",
   user_1: "0xB15DdF55984b39005C64Bc2Cdbc1b38A7bD848E9",
   //
-  usdt_aave: "0x65E2fe35C30eC218b46266F89847c63c2eDa7Dc7",
+  USDT: "0x65E2fe35C30eC218b46266F89847c63c2eDa7Dc7",
   aave_registry: "0x5E52dEc931FFb32f609681B8438A51c675cc232d",
   //
-  aten: "0xc5f57304d4f59e680AaE013e83d4fBd48dE35A8C",
-  arbitrator: "0x05B7D4b7Dc8aE618d83D3f7BF49eb9a716a86bd2",
-  athena: "0xA9826c750d531eE1274B783258f9781459e36Af4",
-  position_manager: "0x4af126738240ebc01dc571e6a8af2c265abb2617",
-  staking_gp: "0x0f5d6f41548584b95c5f26887982b206c02d3f28",
-  policy_manager: "0xc89254dcf3b532ede6a7d2af0a9d674a33a3c20d",
-  protocol_factory: "0xe9c38ede505d553707b6127ff09d41274277c028",
-  staking_policy: "0xf4ac4e74b436ca576df1eac5151e98c2ce26646e",
-  vault: "0x10c931f84a74bc01b9e57c02a74633530fe8944f",
-  claim_manager: "0xe8cfd5f190987b73499aca2e0bf1ba8ba88df1cf",
+  ATEN: "0x1C53B48fd2F92AD86602Aa8CD08CD3317D2FE737",
+  ARBITRATOR: "0x77AB8C2174A770BdbFF9a4eda19C8c4D609A8eA4",
+  ATHENA: "0xd840eadf266502f05B4AC3E30dc1ce7bf293EEc2",
+  POSITIONS_MANAGER: "0x9C66B49072dC17DCBC3ffb4D37DAC6D6Ab3599B5",
+  STAKING_GP: "0x2effe96eBB8EBab29434ffC570a2D8066E2E1A76",
+  POLICY_MANAGER: "0x643891502CE10646Cc2f0d39662c0FCF16C17832",
+  FACTORY_PROTOCOL: "0x5d2C1c0Bcb821dfF5790c46259058DbBf654678F",
+  STAKING_POLICY: "0xc53c128C92A5E99f51BEb0C07896b6c71B6AcCe8",
+  TOKEN_VAULT: "0x6e6c10503302F75C8B267886dE2a93d721e627e3",
+  CLAIM_MANAGER: "0x32F073C3F5b52c326e6fE930e07D0e3388965924",
 };
 
 const amountApprove = BigNumber.from(2).pow(256).sub(1);
@@ -32,6 +32,8 @@ const amountTransfers = BigNumber.from(20_000_000).mul(
 
 async function main() {
   try {
+    await hre.run("compile");
+
     console.log("== STARTING ==");
     const signers = await hre_ethers.getSigners();
     const deployer = signers[0];
@@ -45,12 +47,14 @@ async function main() {
     await ProtocolHelper.deployArbitratorContract(deployer);
 
     const ARBITRATOR_CONTRACT = ProtocolHelper.getArbitratorContract();
-    ADDRESS.arbitrator = ARBITRATOR_CONTRACT.address;
+    ADDRESS.ARBITRATOR = ARBITRATOR_CONTRACT.address;
+
+    console.log("== OK 1 ==");
 
     // =====> deploy athena contracts
     await ProtocolHelper.deployAthenaContract(
       deployer,
-      ADDRESS.usdt_aave,
+      ADDRESS.USDT,
       ADDRESS.aave_registry
     );
     await ProtocolHelper.deployPositionManagerContract(deployer);
@@ -61,10 +65,12 @@ async function main() {
     await ProtocolHelper.deployVaultAtenContract(deployer);
     await ProtocolHelper.deployClaimManagerContract(
       deployer,
-      ADDRESS.arbitrator
+      ADDRESS.ARBITRATOR
     );
 
     await ProtocolHelper.initializeProtocol();
+
+    console.log("== OK 2 ==");
 
     // =====> set fee levels & reward levels
     await ProtocolHelper.setFeeLevelsWithAten(deployer);
@@ -76,27 +82,34 @@ async function main() {
     await ProtocolHelper.addNewProtocolPool("AAVE Lending Pool");
     await ProtocolHelper.addNewProtocolPool("Hotdog Swap Liquidity Pool");
 
+    console.log("== OK 3 ==");
+
     // =====> Make contract interfaces
 
-    const USDT_CONTRACT = new hre_ethers.Contract(ADDRESS.usdt_aave, abiERC20);
+    const USDT_CONTRACT = new hre_ethers.Contract(ADDRESS.USDT, abiERC20);
     const ATEN_CONTRACT = ProtocolHelper.getAtenTokenContract();
     const ATHENA_CONTRACT = ProtocolHelper.getAthenaContract();
     const VAULT_CONTRACT = ProtocolHelper.getVaultAtenContract();
 
-    ADDRESS.athena = ATHENA_CONTRACT.address;
-    ADDRESS.vault = VAULT_CONTRACT.address;
+    ADDRESS.ATHENA = ATHENA_CONTRACT.address;
+    ADDRESS.TOKEN_VAULT = VAULT_CONTRACT.address;
 
     // =====> approve tokens
 
     await USDT_CONTRACT.connect(deployer).approve(
-      ADDRESS.athena,
+      ADDRESS.ATHENA,
       amountApprove
     );
     await ATEN_CONTRACT.connect(deployer).approve(
-      ADDRESS.athena,
+      ADDRESS.ATHENA,
       amountApprove
     );
-    await ATEN_CONTRACT.connect(deployer).approve(ADDRESS.vault, amountApprove);
+    await ATEN_CONTRACT.connect(deployer).approve(
+      ADDRESS.TOKEN_VAULT,
+      amountApprove
+    );
+
+    console.log("== OK 4 ==");
 
     // =====> deposit ATEN in rewards vault & send ATEN to user
 
@@ -105,7 +118,7 @@ async function main() {
     );
 
     await ATEN_CONTRACT.connect(deployer).transfer(
-      ADDRESS.vault,
+      ADDRESS.TOKEN_VAULT,
       amountTransfers
     );
     await ATEN_CONTRACT.connect(deployer).transfer(
@@ -113,7 +126,15 @@ async function main() {
       amountTransfers
     );
 
-    console.log("== OK ==");
+    console.log("====== OK ======");
+
+    console.log(
+      "CONTRACT: ",
+      Object.entries(CONTRACT).reduce(
+        (acc, [key, value]) => ({ ...acc, [key]: value.address }),
+        {}
+      )
+    );
   } catch (err: any) {
     console.log("!!! ERROR !!!");
     console.log(err);
