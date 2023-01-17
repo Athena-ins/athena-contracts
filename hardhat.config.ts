fb17536f8d26e4229210e3cb3d19b2ccfcd893d3
VirtualBlackWhale <victor.brun@ymail.com>
2023-01-17 18:38:44 +0100

    fix(env config): changed required data in env + handling of env config in hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import { ethers } from "ethers";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-ethers";
@@ -19,6 +20,58 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const mainnetForkConfig = {
+  forking: {
+    url: process.env.MAINNET_URL || "",
+    blockNumber: !process.env.FORKING_BLOCK
+      ? undefined
+      : Number(process.env.FORKING_BLOCK),
+  },
+  mining: {
+    auto: true,
+  },
+  accounts: {
+    count: 302,
+  },
+};
+
+const goerliForkConfig = {
+  forking: {
+    url: process.env.GOERLI_URL || "",
+    blockNumber: 8328120,
+  },
+  mining: {
+    auto: true,
+  },
+  accounts: [
+    // Deployer
+    {
+      privateKey: process.env.DEPLOY_TESTNET_PK as string,
+      balance: "10000000000000000000000",
+    },
+    // Users 1,2,3,4
+    {
+      privateKey: (process.env.DEPLOY_TESTNET_PK as string).replace("8", "9"),
+      balance: "10000000000000000000046",
+    },
+    ...Array(300)
+      .fill("")
+      .map((_) => ({
+        privateKey: ethers.Wallet.createRandom().privateKey,
+        balance: "10000000000000000",
+      })),
+  ],
+};
+
+const chooseForkConfig = () => {
+  if (process.env.HARDHAT_FORK_TARGET?.toLowerCase() === "mainnet") {
+    return mainnetForkConfig;
+  } else if (process.env.HARDHAT_FORK_TARGET?.toLowerCase() === "goerli") {
+    return goerliForkConfig;
+  }
+  return goerliForkConfig;
+};
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -32,73 +85,11 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  networks: {
-    // hardhat: {
-    //   forking: {
-    //     url: process.env.MAINNET_URL || "",
-    //     blockNumber: !process.env.FORKING_BLOCK
-    //       ? undefined
-    //       : Number(process.env.FORKING_BLOCK),
-    //   },
-    //   mining: {
-    //     auto: true,
-    //     // interval: 5000,
-    //   },
-    //   accounts: {
-    //     count: 302,
-    //   },
-    // },
-    hardhat: {
-      forking: {
-        url: process.env.GOERLI_URL || "",
-        blockNumber: 8299451,
-      },
-      mining: {
-        auto: true,
-      },
-      accounts: [
-        // Deployer
-        {
-          privateKey: process.env.DEPLOY_TESTNET_PK as string,
-          balance: "10000000000000000000000",
-        },
-        // Users 1,2,3,4
-        {
-          privateKey: (process.env.DEPLOY_TESTNET_PK as string).replace(
-            "8",
-            "9"
-          ),
-          balance: "10000000000000000000046",
-        },
-      ],
-    },
 
-    rinkeby: {
-      url: process.env.RINKEBY_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined
-          ? [process.env.PRIVATE_KEY].concat(
-              process.env.PRIVATE_KEY_2 !== undefined
-                ? [process.env.PRIVATE_KEY_2]
-                : []
-            )
-          : [],
-    },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    kovan: {
-      url: process.env.KOVAN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    mumbai: {
-      url: process.env.MUMBAI_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
+  // ====== Networks ====== //
+
+  networks: {
+    hardhat: chooseForkConfig(),
     mainnet: {
       url: process.env.MAINNET_URL || "",
       accounts:
@@ -114,12 +105,23 @@ const config: HardhatUserConfig = {
             ]
           : [],
     },
+    mumbai: {
+      url: process.env.MUMBAI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
+
+  // ====== Gas Reporter ====== //
+
   gasReporter: {
     enabled: process.env.REPORT_GAS === "true",
     currency: "USD",
     coinmarketcap: process.env.COINMARKETCAP_API_KEY || "",
   },
+
+  // ====== Etherscan ====== //
+
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY ? process.env.ETHERSCAN_API_KEY : "",
   },
