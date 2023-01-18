b1fd04ab7458cb2423d1132102cc8162d5c138a6
VirtualBlackWhale <victor.brun@ymail.com>
2023-01-18 18:51:30 +0100

    fix(tests): major restructuring of staking policy test + uncomment staking GP

--- test/stakingGeneralPool.test.ts
+++ test/stakingGeneralPool.test.ts
@@ -31,96 +31,96 @@ describe("Staking General Pool", function () {
     policyTaker3 = allSigners[102];
 
     await ProtocolHelper.deployAllContractsAndInitializeProtocol(owner);
-    // await ProtocolHelper.addNewProtocolPool("Test protocol 0");
-    // await ProtocolHelper.addNewProtocolPool("Test protocol 1");
-    // await ProtocolHelper.addNewProtocolPool("Test protocol 2");
-    // await ProtocolHelper.addNewProtocolPool("Test protocol 3");
+    await ProtocolHelper.addNewProtocolPool("Test protocol 0");
+    await ProtocolHelper.addNewProtocolPool("Test protocol 1");
+    await ProtocolHelper.addNewProtocolPool("Test protocol 2");
+    await ProtocolHelper.addNewProtocolPool("Test protocol 3");
 
     // ================= Cover Providers ================= //
 
-    // const USDT_amount1 = "4000000";
-    // const ATEN_amount1 = "100";
-    // await ProtocolHelper.deposit(
-    //   liquidityProvider1,
-    //   USDT_amount1,
-    //   ATEN_amount1,
-    //   [0, 2],
-    //   1 * 24 * 60 * 60
-    // );
+    const USDT_amount1 = "4000000";
+    const ATEN_amount1 = "100";
+    await ProtocolHelper.deposit(
+      liquidityProvider1,
+      USDT_amount1,
+      ATEN_amount1,
+      [0, 2],
+      1 * 24 * 60 * 60
+    );
 
-    // const USDT_amount2 = "330";
-    // const ATEN_amount2 = "9000000";
-    // await ProtocolHelper.deposit(
-    //   liquidityProvider2,
-    //   USDT_amount2,
-    //   ATEN_amount2,
-    //   [0, 1, 2],
-    //   1 * 24 * 60 * 60
-    // );
+    const USDT_amount2 = "330";
+    const ATEN_amount2 = "9000000";
+    await ProtocolHelper.deposit(
+      liquidityProvider2,
+      USDT_amount2,
+      ATEN_amount2,
+      [0, 1, 2],
+      1 * 24 * 60 * 60
+    );
 
-    // const USDT_amount3 = "36500";
-    // const ATEN_amount3 = "9000";
-    // await ProtocolHelper.deposit(
-    //   liquidityProvider3,
-    //   USDT_amount3,
-    //   ATEN_amount3,
-    //   [1, 3],
-    //   1 * 24 * 60 * 60
-    // );
+    const USDT_amount3 = "36500";
+    const ATEN_amount3 = "9000";
+    await ProtocolHelper.deposit(
+      liquidityProvider3,
+      USDT_amount3,
+      ATEN_amount3,
+      [1, 3],
+      1 * 24 * 60 * 60
+    );
 
     // ================= Policy Buyers ================= //
 
-    // await HardhatHelper.USDT_maxApprove(
-    //   policyTaker1,
-    //   ProtocolHelper.getAthenaContract().address
-    // );
+    await HardhatHelper.USDT_maxApprove(
+      policyTaker1,
+      ProtocolHelper.getAthenaContract().address
+    );
 
-    // const capital1 = "109500";
-    // const capital1_2 = "140";
-    // const premium1 = "2190";
-    // const atensLocked1 = "0";
-    // await ProtocolHelper.buyPolicies(
-    //   policyTaker1,
-    //   [capital1, capital1_2],
-    //   [premium1, premium1],
-    //   [atensLocked1, atensLocked1],
-    //   [0, 3],
-    //   20 * 24 * 60 * 60
-    // );
+    const capital1 = "109500";
+    const capital1_2 = "140";
+    const premium1 = "2190";
+    const atensLocked1 = "0";
+    await ProtocolHelper.buyPolicies(
+      policyTaker1,
+      [capital1, capital1_2],
+      [premium1, premium1],
+      [atensLocked1, atensLocked1],
+      [0, 3],
+      20 * 24 * 60 * 60
+    );
 
-    // await HardhatHelper.USDT_maxApprove(
-    //   policyTaker2,
-    //   ProtocolHelper.getAthenaContract().address
-    // );
+    await HardhatHelper.USDT_maxApprove(
+      policyTaker2,
+      ProtocolHelper.getAthenaContract().address
+    );
 
-    // const capital2 = "219000";
-    // const premium2 = "8760";
-    // const atensLocked2 = "0";
-    // await ProtocolHelper.buyPolicy(
-    //   policyTaker2,
-    //   capital2,
-    //   premium2,
-    //   atensLocked2,
-    //   0,
-    //   10 * 24 * 60 * 60
-    // );
+    const capital2 = "219000";
+    const premium2 = "8760";
+    const atensLocked2 = "0";
+    await ProtocolHelper.buyPolicy(
+      policyTaker2,
+      capital2,
+      premium2,
+      atensLocked2,
+      0,
+      10 * 24 * 60 * 60
+    );
 
-    // await HardhatHelper.USDT_maxApprove(
-    //   policyTaker3,
-    //   ProtocolHelper.getAthenaContract().address
-    // );
+    await HardhatHelper.USDT_maxApprove(
+      policyTaker3,
+      ProtocolHelper.getAthenaContract().address
+    );
 
-    // const capital3 = "182500";
-    // const premium3 = "8760";
-    // const atensLocked3 = "0";
-    // await ProtocolHelper.buyPolicy(
-    //   policyTaker3,
-    //   capital3,
-    //   premium3,
-    //   atensLocked3,
-    //   2,
-    //   10 * 24 * 60 * 60
-    // );
+    const capital3 = "18250";
+    const premium3 = "8760";
+    const atensLocked3 = "0";
+    await ProtocolHelper.buyPolicy(
+      policyTaker3,
+      capital3,
+      premium3,
+      atensLocked3,
+      1,
+      10 * 24 * 60 * 60
+    );
   });
 
   it("Should check staking rate of liquidity providers", async function () {
--- test/stakingPolicy.test.ts
+++ test/stakingPolicy.test.ts
@@ -1,30 +1,14 @@
-import hre from "hardhat";
-import { BigNumber, Contract, ethers as ethersOriginal } from "ethers";
-import weth_abi from "../abis/weth.json";
-
-import {
-  ATEN_TOKEN,
-  ATEN_OWNER_ADDRESS,
-  USDT,
-  USDT_AAVE_ATOKEN,
-  USDT_Wrong,
-  WETH,
-  AAVE_LENDING_POOL,
-  AAVE_REGISTRY,
-  NULL_ADDRESS,
-  ARBITRATOR_ADDRESS,
-  deployAndInitProtocol,
-  increaseTimeAndMine,
-} from "./helpers";
-
-///
-
 import chai, { expect } from "chai";
 import { ethers as hre_ethers } from "hardhat";
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import chaiAsPromised from "chai-as-promised";
 
 import HardhatHelper from "./helpers/HardhatHelper";
+import type {
+  typeAthena,
+  typeStakingPolicy,
+  typeATEN,
+} from "./helpers/TypedContracts";
 import ProtocolHelper from "./helpers/ProtocolHelper";
 
 chai.use(chaiAsPromised);
@@ -39,228 +23,131 @@ let policyTaker1: ethers.Signer;
 let policyTaker2: ethers.Signer;
 let policyTaker3: ethers.Signer;
 
-let ATHENA_CONTRACT: ethersOriginal.Contract,
-  POS_CONTRACT: ethersOriginal.Contract,
-  STAKED_ATENS_CONTRACT: ethersOriginal.Contract,
-  VAULT_ATENS_CONTRACT: ethersOriginal.Contract,
-  STAKED_ATENS_CONTRACT_POLICY: ethersOriginal.Contract,
-  FACTORY_PROTOCOL_CONTRACT: ethersOriginal.Contract,
-  // AAVELP_CONTRACT: ethersOriginal.Contract,
-  ATEN_TOKEN_CONTRACT: ethersOriginal.Contract = new Contract(
-    ATEN_TOKEN,
-    weth_abi
-  ),
-  POLICY_CONTRACT: ethersOriginal.Contract,
-  allSigners: ethers.Signer[];
-
-const USDT_TOKEN_CONTRACT = new ethers.Contract(USDT, weth_abi);
+let ATEN: typeATEN;
+let ATHENA_CONTRACT: typeAthena;
+let STAKING_POLICY: typeStakingPolicy;
 
 describe("Staking Policy Rewards", function () {
   before(async function () {
-    allSigners = await hre.ethers.getSigners();
-    [
-      ATHENA_CONTRACT,
-      POS_CONTRACT,
-      STAKED_ATENS_CONTRACT,
-      STAKED_ATENS_CONTRACT_POLICY,
-      VAULT_ATENS_CONTRACT,
-    ] = await deployAndInitProtocol(allSigners);
-    console.log(
-      "Address : ",
-      ATHENA_CONTRACT.address,
-      STAKED_ATENS_CONTRACT.address
+    await HardhatHelper.reset();
+    const allSigners = await HardhatHelper.allSigners();
+    owner = allSigners[0];
+    liquidityProvider1 = allSigners[1];
+    liquidityProvider2 = allSigners[2];
+    liquidityProvider3 = allSigners[3];
+    policyTaker1 = allSigners[100];
+    policyTaker2 = allSigners[101];
+    policyTaker3 = allSigners[102];
+
+    await ProtocolHelper.deployAllContractsAndInitializeProtocol(owner);
+    await ProtocolHelper.addNewProtocolPool("Test protocol 0");
+    await ProtocolHelper.addNewProtocolPool("Test protocol 1");
+    await ProtocolHelper.addNewProtocolPool("Test protocol 2");
+    await ProtocolHelper.addNewProtocolPool("Test protocol 3");
+
+    // ================= Cover Providers ================= //
+
+    ATEN = ProtocolHelper.getAtenTokenContract();
+    ATHENA_CONTRACT = ProtocolHelper.getAthenaContract();
+    STAKING_POLICY = ProtocolHelper.getStakedAtensPolicyContract();
+
+    // ================= Cover Providers ================= //
+
+    const USDT_amount1 = "4000000";
+    const ATEN_amount1 = "100";
+    await ProtocolHelper.deposit(
+      liquidityProvider1,
+      USDT_amount1,
+      ATEN_amount1,
+      [0, 2],
+      1 * 24 * 60 * 60
     );
 
-    await hre.network.provider.request({
-      method: "hardhat_impersonateAccount",
-      params: ["0xf977814e90da44bfa03b6295a0616a897441acec"],
-    });
-    const binanceSigner = await hre.ethers.getSigner(
-      "0xF977814e90dA44bFA03b6295A0616a897441aceC"
+    const USDT_amount2 = "330";
+    const ATEN_amount2 = "9000000";
+    await ProtocolHelper.deposit(
+      liquidityProvider2,
+      USDT_amount2,
+      ATEN_amount2,
+      [0, 1, 2],
+      1 * 24 * 60 * 60
     );
 
-    const transfer = await USDT_TOKEN_CONTRACT.connect(binanceSigner).transfer(
-      allSigners[1].getAddress(),
-      ethers.utils.parseUnits("100000", 6)
-    );
-    expect(
-      await USDT_TOKEN_CONTRACT.connect(allSigners[1]).balanceOf(
-        allSigners[1].getAddress()
-      )
-    ).to.be.not.equal(BigNumber.from("0"));
-
-    const transfer2 = await USDT_TOKEN_CONTRACT.connect(binanceSigner).transfer(
-      allSigners[2].getAddress(),
-      ethers.utils.parseUnits("100000", 6)
-    );
-    expect(
-      await USDT_TOKEN_CONTRACT.connect(allSigners[2]).balanceOf(
-        allSigners[2].getAddress()
-      )
-    ).to.equal(ethers.utils.parseUnits("100000", 6));
-
-    const transfer3 = await USDT_TOKEN_CONTRACT.connect(binanceSigner).transfer(
-      allSigners[3].getAddress(),
-      ethers.utils.parseUnits("100000", 6)
-    );
-    expect(
-      await USDT_TOKEN_CONTRACT.connect(allSigners[2]).balanceOf(
-        allSigners[3].getAddress()
-      )
-    ).to.equal(ethers.utils.parseUnits("100000", 6));
-
-    /** ATEN TOKENS  */
-    await hre.network.provider.request({
-      method: "hardhat_impersonateAccount",
-      params: [ATEN_OWNER_ADDRESS],
-    });
-    const atenOwnerSigner = await hre.ethers.getSigner(ATEN_OWNER_ADDRESS);
-    const ATEN_TOKEN_CONTRACT = new ethers.Contract(
-      ATEN_TOKEN,
-      weth_abi,
-      atenOwnerSigner
-    );
-    await ATEN_TOKEN_CONTRACT.transfer(
-      allSigners[0].getAddress(),
-      ethers.utils.parseEther("10000000")
-    );
-    await ATEN_TOKEN_CONTRACT.transfer(
-      allSigners[1].getAddress(),
-      ethers.utils.parseEther("10000000")
-    );
-    await ATEN_TOKEN_CONTRACT.transfer(
-      allSigners[2].getAddress(),
-      ethers.utils.parseEther("10000000")
+    const USDT_amount3 = "36500";
+    const ATEN_amount3 = "9000";
+    await ProtocolHelper.deposit(
+      liquidityProvider3,
+      USDT_amount3,
+      ATEN_amount3,
+      [1, 3],
+      1 * 24 * 60 * 60
     );
 
-    await ATEN_TOKEN_CONTRACT.transfer(
-      allSigners[3].getAddress(),
-      ethers.utils.parseEther("10000000")
-    );
-    await ATEN_TOKEN_CONTRACT.connect(allSigners[1]).approve(
-      STAKED_ATENS_CONTRACT_POLICY.address,
-      ethers.utils.parseEther("10000000")
-    );
-    await ATEN_TOKEN_CONTRACT.connect(allSigners[2]).approve(
-      STAKED_ATENS_CONTRACT_POLICY.address,
-      ethers.utils.parseEther("10000000")
+    // ================= Policy Buyers ================= //
+
+    const capital1 = "109500";
+    const capital1_2 = "140";
+    const premium1 = "2190";
+    const atensLocked1 = "500";
+    await ProtocolHelper.buyPolicies(
+      policyTaker1,
+      [capital1, capital1_2],
+      [premium1, premium1],
+      [atensLocked1, atensLocked1],
+      [0, 3],
+      20 * 24 * 60 * 60
     );
 
-    await ATEN_TOKEN_CONTRACT.connect(allSigners[3]).approve(
-      STAKED_ATENS_CONTRACT_POLICY.address,
-      ethers.utils.parseEther("10000000")
-    );
-    await USDT_TOKEN_CONTRACT.connect(allSigners[1]).approve(
-      ATHENA_CONTRACT.address,
-      ethers.utils.parseEther("10000000")
-    );
-    await USDT_TOKEN_CONTRACT.connect(allSigners[2]).approve(
-      ATHENA_CONTRACT.address,
-      ethers.utils.parseEther("10000000")
-    );
-
-    await USDT_TOKEN_CONTRACT.connect(allSigners[3]).approve(
-      ATHENA_CONTRACT.address,
-      ethers.utils.parseEther("10000000")
-    );
-  });
-
-  it("Should deposit ATENS in Vault", async function () {
-    await ATEN_TOKEN_CONTRACT.connect(allSigners[0]).approve(
-      VAULT_ATENS_CONTRACT.address,
-      ethers.utils.parseEther("10000000")
-    );
-    await VAULT_ATENS_CONTRACT.connect(allSigners[0]).deposit(
-      ethers.utils.parseEther("100000")
-    );
-  });
-
-  it("Should set new active Protocol 0", async function () {
-    const tx = await ATHENA_CONTRACT.connect(allSigners[0]).addNewProtocol(
-      "Test protocol 0",
-      30,
-      [],
-      "bafybeiafebm3zdtzmn5mcquacgd47enhsjnebvegnzfunbbbbbbbbbbbbb"
-    );
-    expect(tx).to.haveOwnProperty("hash");
-    const prot = await ATHENA_CONTRACT.connect(allSigners[0]).protocolsMapping(
-      0
-    );
-    expect(prot.name).to.equal("Test protocol 0");
-    const tx1bis = await ATHENA_CONTRACT.connect(allSigners[0]).addNewProtocol(
-      "Test protocol 1",
-      30,
-      [],
-      "bafybeiafebm3zdtzmn5mcquacgd47enhsjnebvegnzfunbbbbbbbbbbbbb"
-    );
-
-    const tx2 = await ATHENA_CONTRACT.connect(
-      allSigners[0]
-    ).setStakingRewardRates([
-      [0, 1_000],
-      [10_000, 1_200],
-      [100_000, 1_600],
-      [1_000_000, 2_000],
-    ]);
-  });
-
-  it("should deposit capital for policy", async function () {
-    const deposit = await ATHENA_CONTRACT.connect(allSigners[1]).deposit(
-      ethers.utils.parseUnits("10000", 6),
+    const capital2 = "219000";
+    const premium2 = "8760";
+    const atensLocked2 = "30000";
+    await ProtocolHelper.buyPolicy(
+      policyTaker2,
+      capital2,
+      premium2,
+      atensLocked2,
       0,
-      [0, 1]
+      10 * 24 * 60 * 60
     );
-    await deposit.wait();
-    expect(deposit).to.haveOwnProperty("hash");
-  });
-
-  it.skip("Should buy Policy", async function () {
-    const policy = await ATHENA_CONTRACT.connect(allSigners[2]).buyPolicy(
-      ethers.utils.parseUnits("1000", 6),
-      ethers.utils.parseUnits("100", 6),
-      0,
-      0
-    );
-    await policy.wait();
-    expect(policy).to.haveOwnProperty("hash");
   });
 
   it("Should fail to buy Policy with Atens cause too many ATENS", async function () {
     await expect(
-      ATHENA_CONTRACT.connect(allSigners[2]).buyPolicy(
-        ethers.utils.parseUnits("1000", 6),
-        ethers.utils.parseUnits("100", 6),
-        ethers.utils.parseEther("20000"),
-        0
+      ATHENA_CONTRACT.connect(policyTaker1).buyPolicies(
+        [1000],
+        [100],
+        [20000],
+        [0]
       )
     ).to.eventually.be.rejectedWith("Too many ATENS");
   });
 
   it("Should buy Policy with Atens", async function () {
-    const policy = await ATHENA_CONTRACT.connect(allSigners[2]).buyPolicy(
-      ethers.utils.parseUnits("1000", 6),
-      ethers.utils.parseUnits("100", 6),
-      ethers.utils.parseEther("10000"),
-      0
+    const policy = await ATHENA_CONTRACT.connect(policyTaker1).buyPolicies(
+      [1000],
+      [100],
+      [10000],
+      [0]
     );
     await policy.wait();
     expect(policy).to.haveOwnProperty("hash");
   });
 
   it("Should buy Policy 2 with Atens", async function () {
-    const policy = await ATHENA_CONTRACT.connect(allSigners[2]).buyPolicy(
-      ethers.utils.parseUnits("1000", 6),
-      ethers.utils.parseUnits("100", 6),
-      ethers.utils.parseEther("6000"),
-      1
+    const policy = await ATHENA_CONTRACT.connect(policyTaker1).buyPolicies(
+      [1000],
+      [100],
+      [6000],
+      [1]
     );
     await policy.wait();
     expect(policy).to.haveOwnProperty("hash");
   });
+
   it("Should return remaining lock time ", async function () {
-    const userStakes = await STAKED_ATENS_CONTRACT_POLICY.connect(
-      allSigners[2]
-    ).userStakes(allSigners[2].getAddress());
+    const userStakes = await STAKING_POLICY.connect(
+      policyTaker1
+    ).allAccountStakingPositions(await policyTaker1.getAddress());
     console.log("User stakes", userStakes);
 
     expect(userStakes[1].timestamp.toNumber()).to.not.equal(0);
@@ -268,72 +155,58 @@ describe("Staking Policy Rewards", function () {
 
   it("Should reject invalid withdraw Atens amount", async function () {
     await expect(
-      ATHENA_CONTRACT.connect(allSigners[2]).withdrawAtensPolicy(
-        ethers.utils.parseEther("20000"),
-        1
-      )
+      ATHENA_CONTRACT.connect(policyTaker1).withdrawAtensPolicy(1)
     ).to.eventually.be.rejectedWith("Invalid amount");
   });
 
   it("Should lock ATENS on 1 year", async function () {
     await expect(
-      ATHENA_CONTRACT.connect(allSigners[2]).withdrawAtensPolicy(
-        ethers.utils.parseEther("10000"),
-        0
-      )
+      ATHENA_CONTRACT.connect(policyTaker1).withdrawAtensPolicy(0)
     ).to.eventually.be.rejectedWith("Locked window");
   });
 
   it("Expect 12 months rewards for 100% APR", async function () {
-    const rewards = await STAKED_ATENS_CONTRACT_POLICY.connect(
-      allSigners[2]
-    ).rewardsOf(allSigners[2].getAddress(), 0);
-    expect(rewards.toNumber()).to.be.lessThanOrEqual(
-      ethers.utils.parseEther("0.001").toNumber()
+    const rewards = await STAKING_POLICY.connect(policyTaker1).rewardsOf(
+      await policyTaker1.getAddress(),
+      0
     );
-    increaseTimeAndMine(60 * 60 * 24 * 365);
-    const rewards2 = await STAKED_ATENS_CONTRACT_POLICY.connect(
-      allSigners[2]
-    ).rewardsOf(allSigners[2].getAddress(), 0);
-    expect(rewards2.toString()).to.equal(
-      ethers.utils.parseEther("10000").toString()
+    expect(rewards.toNumber()).to.be.lessThanOrEqual(0.001);
+
+    await HardhatHelper.setNextBlockTimestamp(60 * 60 * 24 * 365 + 10);
+
+    const rewards2 = await STAKING_POLICY.connect(policyTaker1).rewardsOf(
+      await policyTaker1.getAddress(),
+      0
     );
+
+    expect(rewards2.toString()).to.equal(10000);
   });
+
   it("Should return 2 staking Policy ", async function () {
-    const indexUser = await STAKED_ATENS_CONTRACT_POLICY.connect(
-      allSigners[2]
-    ).stakes(allSigners[2].getAddress());
-    expect(indexUser.index.toNumber()).to.equal(2);
+    const indexUser = await STAKING_POLICY.connect(
+      policyTaker1
+    ).allAccountStakingPositions(await policyTaker1.getAddress());
+
+    expect(indexUser.length).to.equal(2);
   });
+
   it("Should unlock ATENS and withdraw after 1 year", async function () {
-    await ATEN_TOKEN_CONTRACT.connect(allSigners[0]).transfer(
-      STAKED_ATENS_CONTRACT_POLICY.address,
-      ethers.utils.parseEther("100")
+    await ATEN.connect(policyTaker2).transfer(STAKING_POLICY.address, 100);
+    await ATEN.connect(policyTaker2).transfer(STAKING_POLICY.address, 100);
+    const balBefore = await ATEN.connect(policyTaker2).balanceOf(
+      await policyTaker1.getAddress()
     );
-    await ATEN_TOKEN_CONTRACT.connect(allSigners[0]).transfer(
-      STAKED_ATENS_CONTRACT_POLICY.address,
-      ethers.utils.parseEther("100")
-    );
-    const balBefore = await ATEN_TOKEN_CONTRACT.connect(
-      allSigners[0]
-    ).balanceOf(allSigners[2].getAddress());
     await expect(
-      ATHENA_CONTRACT.connect(allSigners[2]).withdrawAtensPolicy(
-        ethers.utils.parseEther("10000"),
-        0
-      )
+      ATHENA_CONTRACT.connect(policyTaker1).withdrawAtensPolicy(0)
     ).to.eventually.haveOwnProperty("hash");
-    const balAfter = await ATEN_TOKEN_CONTRACT.connect(allSigners[0]).balanceOf(
-      allSigners[2].getAddress()
+    const balAfter = await ATEN.connect(policyTaker2).balanceOf(
+      await policyTaker1.getAddress()
     );
     expect(balAfter.sub(balBefore).toString()).to.equal(
-      ethers.utils.parseEther("10000").mul(99975).div(100000).mul(2).toString()
+      BigNumber.from(10000).mul(99975).div(100000).mul(2).toString()
     );
     await expect(
-      ATHENA_CONTRACT.connect(allSigners[2]).withdrawAtensPolicy(
-        ethers.utils.parseEther("6000"),
-        1
-      )
+      ATHENA_CONTRACT.connect(policyTaker1).withdrawAtensPolicy(1)
     ).to.eventually.haveOwnProperty("hash");
   });
 });
