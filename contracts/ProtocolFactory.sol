b3282714ee878ef0f22933d6c28b27ec0c8b7982
VirtualBlackWhale <victor.brun@ymail.com>
2023-02-07 19:23:47 +0100

    feat(pool factory): shift management of pool data from core to pool factory + add views + update interface

--- contracts/ProtocolFactory.sol
+++ contracts/ProtocolFactory.sol
@@ -1,49 +1,147 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
-import "./ProtocolPool.sol";
+
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract ProtocolFactory {
-  address private immutable core;
-  uint256 public immutable commitDelay;
+import "./ProtocolPool.sol";
+
+import "./interfaces/IProtocolFactory.sol";
+
+contract ProtocolFactory is IProtocolFactory {
+  address public immutable core;
   address public immutable policyManager;
 
-  constructor(
-    address _core,
-    address policyManager_,
-    uint256 _commitDelay
-  ) {
-    core = _core;
+  uint128 public nextPoolId;
+
+  mapping(uint128 => mapping(uint128 => bool)) public incompatibilityProtocols;
+  mapping(uint128 => Protocol) public protocolsMapping;
+
+  constructor(address core_, address policyManager_) {
+    core = core_;
     policyManager = policyManager_;
-    commitDelay = _commitDelay;
   }
 
+  /// ========================= ///
+  /// ========= EVENTS ======== ///
+  /// ========================= ///
+
+  error OnlyCore();
+  error OutOfRange();
+  error ProtocolIsInactive();
+  error SamePoolIds();
+  error IncompatibleProtocol(uint256, uint256);
+
+  /// ========================= ///
+  /// ========= EVENTS ======== ///
+  /// ========================= ///
+
+  event NewProtocol(uint128);
+
+  /// ============================ ///
+  /// ========= MODIFIERS ======== ///
+  /// ============================ ///
+
   modifier onlyCore() {
-    require(msg.sender == core, "Only Core");
+    if (msg.sender != core) revert OnlyCore();
     _;
   }
 
+  /// ======================== ///
+  /// ========= VIEWS ======== ///
+  /// ======================== ///
+
+  function getNextPoolId() external view returns (uint128) {
+    return nextPoolId;
+  }
+
+  function getPoolAddress(uint128 poolId_) external view returns (address) {
+    return protocolsMapping[poolId_].deployed;
+  }
+
+  function getPool(uint128 poolId_) external view returns (Protocol memory) {
+    return protocolsMapping[poolId_];
+  }
+
+  /// ================================= ///
+  /// ========== DEPLOY POOL ========== ///
+  /// ================================= ///
+
+  function validePoolIds(uint128[] calldata poolIds) external view {
+    for (uint256 firstIndex = 0; firstIndex < poolIds.length; firstIndex++) {
+      Protocol memory firstProtocol = protocolsMapping[poolIds[firstIndex]];
+
+      if (firstProtocol.paused == true) revert ProtocolIsInactive();
+
+      for (
+        uint256 secondIndex = firstIndex + 1;
+        secondIndex < poolIds.length;
+        secondIndex++
+      ) {
+        if (poolIds[firstIndex] == poolIds[secondIndex]) revert SamePoolIds();
+
+        bool isIncompatible = incompatibilityProtocols[poolIds[firstIndex]][
+          poolIds[secondIndex]
+        ];
+        bool isIncompatibleReverse = incompatibilityProtocols[
+          poolIds[secondIndex]
+        ][poolIds[firstIndex]];
+
+        if (isIncompatible == true || isIncompatibleReverse == true)
+          revert IncompatibleProtocol(firstIndex, secondIndex);
+      }
+    }
+  }
+
   function deployProtocol(
-    address stablecoin,
-    uint128 newPoolId,
-    uint256 _uOptimal,
-    uint256 _r0,
-    uint256 _rSlope1,
-    uint256 _rSlope2
-  ) external onlyCore returns (address) {
-    return
-      address(
-        new ProtocolPool(
-          core,
-          policyManager,
-          stablecoin,
-          newPoolId,
-          _uOptimal,
-          _r0,
-          _rSlope1,
-          _rSlope2,
-          commitDelay
-        )
-      );
+    address stablecoin_, // @bw variable here but hardcoded in core & other contracts
+    string calldata name_,
+    uint128[] calldata incompatiblePools_,
+    uint128 commitDelay_,
+    uint256 uOptimal_,
+    uint256 r0_,
+    uint256 rSlope1_,
+    uint256 rSlope2_
+  ) external onlyCore returns (uint128 poolId) {
+    poolId = nextPoolId;
+    nextPoolId++;
+
+    address deployedAt = address(
+      new ProtocolPool(
+        poolId,
+        core,
+        policyManager,
+        stablecoin_,
+        commitDelay_,
+        uOptimal_,
+        r0_,
+        rSlope1_,
+        rSlope2_
+      )
+    );
+
+    for (uint256 i = 0; i < incompatiblePools_.length; i++) {
+      incompatibilityProtocols[poolId][incompatiblePools_[i]] = true;
+    }
+
+    protocolsMapping[poolId] = Protocol({
+      id: poolId,
+      name: name_,
+      deployed: deployedAt,
+      paused: false,
+      claimsOngoing: 0,
+      stablecoin: stablecoin_,
+      commitDelay: commitDelay_
+    });
+
+    emit NewProtocol(poolId);
+  }
+
+  /// =========================== ///
+  /// ========== ADMIN ========== ///
+  /// =========================== ///
+
+  // @bw unimplemented (+ ongoing claims)
+  function pauseProtocol(uint128 poolId, bool pause) external onlyCore {
+    protocolsMapping[poolId].paused = pause;
   }
 }
--- contracts/interfaces/IProtocolFactory.sol
+++ contracts/interfaces/IProtocolFactory.sol
@@ -2,12 +2,32 @@
 pragma solidity ^0.8;
 
 interface IProtocolFactory {
+  struct Protocol {
+    uint128 id; //id in mapping
+    uint128 claimsOngoing; // claim ongoing, lock funds when claim is ongoing
+    uint128 commitDelay;
+    bool paused; //is Active or paused
+    string name; //Protocol name
+    address stablecoin;
+    address deployed; //Protocol Pool Address deployed
+  }
+
+  function getNextPoolId() external view returns (uint128);
+
+  function getPool(uint128 poolId_) external view returns (Protocol memory);
+
+  function getPoolAddress(uint128 poolId_) external view returns (address);
+
+  function validePoolIds(uint128[] calldata poolIds) external view;
+
   function deployProtocol(
     address stablecoin,
-    uint128 newPoolId,
-    uint256 _uOptimal,
-    uint256 _r0,
-    uint256 _rSlope1,
-    uint256 _rSlope2
-  ) external returns (address _protocolDeployed);
+    string calldata name,
+    uint128[] calldata incompatiblePools,
+    uint128 commitDelay,
+    uint256 uOptimal,
+    uint256 r0,
+    uint256 rSlope1,
+    uint256 rSlope2
+  ) external returns (uint128 poolId);
 }
