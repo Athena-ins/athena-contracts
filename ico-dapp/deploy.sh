54cc7368ad6405ee56496cc8671087f772f56383
VirtualBlackWhale <victor.brun@ymail.com>
2023-02-10 15:22:09 +0100

    fix(token sale ui): move ICO & presale UI from repo to athena-token-sale repo

--- ico-dapp/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-npm run build && \
-tar -cvf ./deploy-athena-ico.tar --exclude='*.map' ./captain-definition ./build/*
-#ssh cpecopristo@captain.dev.copristo.com "caprover deploy -t deploy-athena-ico.tar"
