b5e31e83dbd1eeb01d65fd5558a6a1ef38dbd75e
VirtualBlackWhale <victor.brun@ymail.com>
2023-02-10 15:55:10 +0100

    fix(tests): bad timestamps + remove outdated deploy addresses from readme + update cache

--- test/ongoingCoveragePolicies.test.ts
+++ test/ongoingCoveragePolicies.test.ts
@@ -107,10 +107,7 @@ describe("Ongoing coverage policies", () => {
     expect(result[0].premiumLeft).to.be.equals(2094);
     expect(result[0].dailyCost).to.be.equals(9);
     // @dev value is inconsistent
-    expect(
-      result[0].beginCoveredTime.toNumber() === 1675877568 ||
-        result[0].beginCoveredTime.toNumber() === 1675877569
-    ).to.be.equals(true);
+    expect(result[0].beginCoveredTime).to.be.equals(1675877570);
     expect(result[0].remainingDuration).to.be.equals(20102400);
     expect(result[0].poolId).to.be.equals(0);
 
@@ -119,7 +116,7 @@ describe("Ongoing coverage policies", () => {
     expect(result[1].premiumDeposit).to.be.equals(8760);
     expect(result[1].premiumLeft).to.be.equals(8760);
     expect(result[1].dailyCost).to.be.equals(18);
-    expect(result[1].beginCoveredTime).to.be.equals(1676827975);
+    expect(result[1].beginCoveredTime).to.be.equals(1676827977);
     expect(result[1].remainingDuration).to.be.equals(42048000);
     expect(result[1].poolId).to.be.equals(2);
   });
