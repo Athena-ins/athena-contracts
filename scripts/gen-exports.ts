8064466e4bb2d15ff0cd64b87ed0853be52fd582
VirtualBlackWhale <victor.brun@ymail.com>
2023-02-24 19:17:54 +0100

    feat(gen exports): add export types to api + minor test bug fix

--- scripts/gen-exports.ts
+++ scripts/gen-exports.ts
@@ -3,38 +3,27 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const writeToClientFolder = process.env.EXPORT_TO_CLIENT === "true";
-console.log("writeToClientFolder: ", writeToClientFolder, "\n");
 
 const abiPath = "artifacts/contracts/";
-const rootPathExport = "./uiExport";
-const abiPathExport = writeToClientFolder
-  ? "../athena-dapp/src/data/ABI/"
-  : "./uiExport/ABI/";
-const typechainPathExport = writeToClientFolder
-  ? "../athena-dapp/src/types/typechain/"
-  : "./uiExport/typechain/";
-
-// { common_name: subpath/?filename }
-const targetList = {
-  Athena: "Athena",
-  AtenToken: "erc20/ATEN",
-  UsdtToken: "erc20/USDT",
-  ClaimManager: "ClaimManager",
-  PolicyManager: "PolicyManager",
-  PositionsManager: "PositionsManager",
-  StakingGeneralPool: "StakingGeneralPool",
-  StakingPolicy: "StakingPolicy",
-  TokenVault: "TokenVault",
-  PriceOracleV1: "PriceOracleV1",
-};
+const rootPathExport = "typeExports";
 
 let abiIndex = "";
 let typechainIndex = "";
 
-const main = async () => {
+const genExports = async (
+  targetList: { [key: string]: string },
+  folder?: string,
+  abiPathExport?: string,
+  typechainPathExport?: string
+) => {
+  abiPathExport ??= `typeExports/ABI/${folder || ""}`;
+  typechainPathExport ??= `typeExports/typechain/${folder || ""}`;
+
   if (writeToClientFolder) {
-    if (!fs.existsSync(abiPathExport) || !fs.existsSync(typechainPathExport))
-      throw Error("Client folder not found");
+    if (!fs.existsSync(abiPathExport))
+      throw Error("Client ABI folder not found");
+    if (!fs.existsSync(typechainPathExport))
+      throw Error("Client types folder not found");
   } else {
     if (!fs.existsSync(rootPathExport)) fs.mkdirSync(rootPathExport);
     if (!fs.existsSync(abiPathExport)) fs.mkdirSync(abiPathExport);
@@ -46,7 +35,6 @@ const main = async () => {
     console.log("file: ", file);
 
     // ABI
-
     const abiJson = fs.readFileSync(
       `${abiPath}${path}.sol/${file}.json`,
       "utf8"
@@ -60,7 +48,6 @@ const main = async () => {
     abiIndex += `import ${name}ABI from "./${name}.json";\n`;
 
     // Typechain
-
     fs.copyFileSync(
       `./typechain/${file}.d.ts`,
       `${typechainPathExport}${file}.d.ts`
@@ -76,11 +63,44 @@ const main = async () => {
   fs.writeFileSync(`${abiPathExport}index.ts`, abiIndex);
   fs.writeFileSync(`${typechainPathExport}index.ts`, typechainIndex);
 
+  abiIndex = "";
+  typechainIndex = "";
+
   if (writeToClientFolder) {
-    console.log("\n=> Files copied to athena-dapp\n");
+    console.log(`\n=> Files copied to ${folder}\n`);
   } else {
-    console.log("\n=> Folders ready at uiExport/\n");
+    console.log("\n=> Folders ready at typeExports/\n");
   }
 };
 
-main();
+// { common_name: subpath/?filename }
+const targetListForUi = {
+  Athena: "Athena",
+  AtenToken: "erc20/ATEN",
+  UsdtToken: "erc20/USDT",
+  ClaimManager: "ClaimManager",
+  PolicyManager: "PolicyManager",
+  PositionsManager: "PositionsManager",
+  StakingGeneralPool: "StakingGeneralPool",
+  StakingPolicy: "StakingPolicy",
+  TokenVault: "TokenVault",
+  PriceOracleV1: "PriceOracleV1",
+};
+genExports(
+  targetListForUi,
+  "ui",
+  "../athena-dapp/src/data/ABI/",
+  "../athena-dapp/src/types/typechain/"
+);
+
+const targetListForApi = {
+  Athena: "Athena",
+  ClaimManager: "ClaimManager",
+  CoverManager: "PolicyManager",
+};
+genExports(
+  targetListForApi,
+  "api",
+  "../athena-api/src/abis/",
+  "../athena-api/src/types/typechain/"
+);
