d5c0a1c2c641d5c01c3cd42a22d954af3c153946
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2023-11-28 12:54:13 +0100

    feat (contract staking): add fee levels fns + implement check

--- src/rewards/Staking.sol
+++ src/rewards/Staking.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.20;
 
+// contracts
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
 // libraries
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -10,6 +13,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { IERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 import { IFarmingRange } from "./interfaces/IFarmingRange.sol";
 import { IStaking } from "./interfaces/IStaking.sol";
+import { ILiquidityManager } from "../interfaces/ILiquidityManager.sol";
 
 //======== ERRORS ========//
 
@@ -19,6 +23,8 @@ error FarmingCampaignNotInitialized();
 error UserAlreadyCalledDepositOrWithdrawThisBlock();
 // Aten token is not defined
 error AtenTokenIsNotDefined();
+// Liquidity manager is not defined
+error LiquidityManagerIsNotDefined();
 // Farming is not defined
 error FarmingIsNotDefined();
 // Farming campaign already initialized
@@ -38,7 +44,7 @@ error NoSharesToWithdraw();
  * @custom:source Inspired by ERC-4626 Tokenized Vault Standard
  * @custom:url https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC4626.sol
  */
-contract Staking is IStaking, ERC20 {
+contract Staking is IStaking, ERC20, Ownable {
   using SafeERC20 for IERC20;
 
   uint256 public constant CAMPAIGN_ID = 0;
@@ -51,6 +57,8 @@ contract Staking is IStaking, ERC20 {
   mapping(address => UserInfo) public userInfo;
   uint256 public totalShares;
   bool public farmingInitialized = false;
+
+  ILiquidityManager public liquidityManager;
 
   modifier isFarmingInitialized() {
     if (farmingInitialized == false) {
@@ -68,25 +76,33 @@ contract Staking is IStaking, ERC20 {
   }
 
   constructor(
+    address _owner,
     IERC20 _stakedToken,
-    IFarmingRange _farming
-  ) ERC20("Staked Aten Token", "stAOE") {
+    IFarmingRange _farming,
+    ILiquidityManager liquidityManager_
+  ) ERC20("Staked Aten Token", "stAOE") Ownable(_owner) {
     if (address(_stakedToken) == address(0)) {
       revert AtenTokenIsNotDefined();
     }
     if (address(_farming) == address(0)) {
       revert FarmingIsNotDefined();
     }
+    if (address(liquidityManager) == address(0)) {
+      revert LiquidityManagerIsNotDefined();
+    }
 
     stakedToken = _stakedToken;
     farming = _farming;
+    liquidityManager = liquidityManager_;
   }
 
   /// @inheritdoc IStaking
-  function initializeFarming() external {
+  function initializeFarming(FeeLevel[] memory feeLevels) external {
     if (farmingInitialized == true) {
       revert FarmingCampaignAlreadyInitialized();
     }
+
+    setFeeLevelsWithAten(feeLevels);
 
     _approve(address(this), address(farming), 1 wei);
     _mint(address(this), 1 wei);
@@ -126,6 +142,11 @@ contract Staking is IStaking, ERC20 {
       msg.sender,
       address(this),
       _depositAmount
+    );
+
+    _updateAccountFeeDiscount(
+      userInfo[msg.sender].shares,
+      _currentBalance
     );
 
     emit Deposit(msg.sender, _depositAmount, _userNewShares);
@@ -177,6 +198,11 @@ contract Staking is IStaking, ERC20 {
     totalShares -= _sharesAmount;
     stakedToken.safeTransfer(_to, _tokensToWithdraw);
 
+    _updateAccountFeeDiscount(
+      userInfo[msg.sender].shares,
+      _currentBalance
+    );
+
     emit Withdraw(msg.sender, _to, _tokensToWithdraw, _sharesAmount);
   }
 
@@ -198,6 +224,9 @@ contract Staking is IStaking, ERC20 {
     totalShares -= _sharesAmount;
     userInfo[msg.sender].shares = 0;
     stakedToken.safeTransfer(_to, _tokensToWithdraw);
+
+    feeDiscountOf[msg.sender] = 0;
+    liquidityManager.feeDiscountUpdate(msg.sender, 0);
 
     emit EmergencyWithdraw(
       msg.sender,
@@ -266,5 +295,108 @@ contract Staking is IStaking, ERC20 {
     tokens_ = totalShares != 0
       ? (_shares * _currentBalance) / totalShares
       : _shares / SHARES_FACTOR;
+  }
+
+  //======== FEE DISCOUNT ========//
+
+  struct FeeLevel {
+    uint256 atenAmount;
+    uint256 feeDiscount; // (10_000 = 100% discount)
+  }
+  // Performance fee discount levels
+  FeeLevel[] public feeLevels;
+
+  mapping(address account => uint256 feeDiscount)
+    public feeDiscountOf;
+
+  function _updateAccountFeeDiscount(
+    uint256 _shares,
+    uint256 _currentBalance
+  ) internal {
+    uint256 tokenBalance = _sharesToTokens(_shares, _currentBalance);
+
+    uint256 feeDiscount = feeDiscountOf[msg.sender];
+    uint256 newFeeDiscount = amountToFeeDiscount(tokenBalance);
+
+    if (feeDiscount != newFeeDiscount) {
+      feeDiscountOf[msg.sender] = newFeeDiscount;
+      liquidityManager.feeDiscountUpdate(msg.sender, newFeeDiscount);
+    }
+  }
+
+  /** @notice
+   * Gets all the cover supply fee levels according to the amount of staked ATEN.
+   * @return levels all the fee levels
+   **/
+  function getSupplyFeeLevels()
+    public
+    view
+    returns (FeeLevel[] memory levels)
+  {
+    uint256 nbLevels = supplyFeeLevels.length;
+    levels = new FeeLevel[](nbLevels);
+
+    for (uint256 i = 0; i < nbLevels; i++) {
+      levels[i] = supplyFeeLevels[i];
+    }
+  }
+
+  /** @notice
+   * Retrieves the fee discount according to amount of staked ATEN.
+   * @dev Returns displays warning but levels require an amountAten of 0
+   * @param stakedAten_ amount of ATEN the user stakes in GP
+   * @return _ amount of fees applied to cover supply interests
+   **/
+  function amountToFeeDiscount(
+    uint256 stakedAten_
+  ) public view returns (uint128) {
+    // Lazy check to avoid loop if user doesn't stake
+    if (stakedAten_ == 0) return supplyFeeLevels[0].feeDiscount;
+
+    // Inversed loop starts with the end to find adequate level
+    for (uint256 i = supplyFeeLevels.length - 1; 0 <= i; i--) {
+      // Rate level with atenAmount of 0 will always be true
+      if (supplyFeeLevels[i].atenAmount <= stakedAten_)
+        return supplyFeeLevels[i].feeDiscount;
+    }
+
+    return supplyFeeLevels[0].feeDiscount;
+  }
+
+  /** @notice
+   * Set the fee levels on cover interests according to amount of staked ATEN in general pool.
+   * @dev Levels must be in ascending order of atenAmount
+   * @dev The atenAmount indicates the upper limit for the level
+   * @param levels_ array of fee level structs
+   **/
+  function setFeeLevelsWithAten(
+    FeeLevel[] calldata levels_
+  ) public onlyOwner {
+    // First clean the storage
+    // @bw does not clear storage in classic manner, need to check
+    delete supplyFeeLevels;
+
+    // Set all cover supply fee levels
+    uint256 previousAtenAmount = 0;
+    for (uint256 i = 0; i < levels_.length; i++) {
+      FeeLevel calldata level = levels_[i];
+
+      if (i == 0) {
+        // Require that the first level indicates fees for atenAmount 0
+        if (level.atenAmount != 0) revert MissingBaseRate();
+      } else {
+        // If it isn't the first item check that items are ascending
+        if (level.atenAmount < previousAtenAmount)
+          revert MustSortInAscendingOrder();
+
+        previousAtenAmount = level.atenAmount;
+      }
+
+      // Check that fee discount is not higher than 100%
+      if (10_000 < level.feeDiscount) revert GreaterThan100Percent();
+
+      // save to storage
+      supplyFeeLevels.push(level);
+    }
   }
 }
