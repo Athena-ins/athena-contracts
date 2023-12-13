40e3e17b33fff0a270b05d01dc5bd2c1211ca05e
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2023-12-13 16:25:35 +0100

    fix (hardhat): config blocking typing gen

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -141,7 +141,6 @@ const config: HardhatUserConfig = {
     outDir: "typechain",
     target: "ethers-v5",
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
-    externalArtifacts: [], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
   },
 
   // ====== Paths ====== //
