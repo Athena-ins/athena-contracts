cad84f58da901cf99256520eb9320b90643168f8
Blackwhale <victor.blackwhale@gmail.com>
2023-12-20 22:38:41 +0100

    feat (strat): setup basic strat manager

--- src/managers/StrategyManager.sol
+++ src/managers/StrategyManager.sol
@@ -12,6 +12,7 @@ import { RayMath } from "../libs/RayMath.sol";
 
 // interfaces
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { ILendingPool } from "../interfaces/ILendingPool.sol";
 
 //======== ERRORS ========//
 
@@ -25,15 +26,15 @@ contract StrategyManagerV0 is Ownable {
   //======== STORAGE ========//
   address public liquidityManager;
 
-  address public depositContract =
-    0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9; // AAVE lending pool v2
-  address public underlyingAsset =
-    0xdAC17F958D2ee523a2206206994597C13D831ec7; // USDT
-  address public liquidityAsset =
-    0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811; // aUSDT v2
+  ILendingPool public aaveLendingPool =
+    ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9); // AAVE lending pool v2
+  address public usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // USDT
+  address public ausdt = 0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811; // aUSDT v2
 
   struct PositionData {
     uint256 amountUnderlying;
+    uint256 startRewardRate;
+    uint256 accumulatedRewards;
   }
 
   mapping(uint256 _tokenId => PositionData _data) public positionData;
@@ -53,12 +54,31 @@ contract StrategyManagerV0 is Ownable {
 
   //======== FUNCTIONS ========//
 
+  /**
+   * @notice Returns the current rate between aToken and underlying token
+   * @return uint256 The current reward index in rays
+   *
+   * @dev A reward rate of 1e27 means 1 aToken = 1 underlying token
+   */
+  function getRewardRate() public view returns (uint256) {
+    return aaveLendingPool.getReserveNormalizedIncome(usdt);
+  }
+
   function depositToStrategy(
     uint256 strategyId_,
     uint256 tokenId_,
     uint256 amountUnderlying_
   ) external onlyLiquidityManager {
     if (strategyId_ != 0) revert NotAValidStrategy();
+
+    IERC20(usdt).forceApprove(address(this), amountUnderlying_);
+
+    aaveLendingPool.deposit(
+      usdt,
+      amountUnderlying_,
+      address(this),
+      0
+    );
   }
 
   function withdrawFromStrategy(
@@ -69,6 +89,9 @@ contract StrategyManagerV0 is Ownable {
     uint256 feeDiscount_
   ) external onlyLiquidityManager {
     if (strategyId_ != 0) revert NotAValidStrategy();
+
+    // No need to approve aToken since they are burned in pool
+    aaveLendingPool.withdraw(usdt, amountUnderlying_, account_);
   }
 
   function payoutFromStrategy(
@@ -77,6 +100,9 @@ contract StrategyManagerV0 is Ownable {
     address account_
   ) external onlyLiquidityManager {
     if (strategyId_ != 0) revert NotAValidStrategy();
+
+    // No need to approve aToken since they are burned in pool
+    aaveLendingPool.withdraw(usdt, amountUnderlying_, account_);
   }
 
   //======== ADMIN ========//
