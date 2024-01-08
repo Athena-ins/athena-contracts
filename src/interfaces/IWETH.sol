0a92e05a6cee16473e8119cf786a06607a29e951
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2024-01-08 17:05:40 +0100

    feat (interfaces): add weth & uni v2 router

---
+++ src/interfaces/IWETH.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IWETH {
+  function name() external view returns (string memory);
+
+  function symbol() external view returns (string memory);
+
+  function decimals() external view returns (uint8);
+
+  event Approval(
+    address indexed src,
+    address indexed guy,
+    uint256 wad
+  );
+  event Transfer(
+    address indexed src,
+    address indexed dst,
+    uint256 wad
+  );
+  event Deposit(address indexed dst, uint256 wad);
+  event Withdrawal(address indexed src, uint256 wad);
+
+  function balanceOf(address account) external view returns (uint256);
+
+  function allowance(
+    address src,
+    address guy
+  ) external view returns (uint256);
+
+  function totalSupply() external view returns (uint256);
+
+  function deposit() external payable;
+
+  function withdraw(uint256 wad) external;
+
+  function approve(address guy, uint256 wad) external returns (bool);
+
+  function transfer(address dst, uint256 wad) external returns (bool);
+
+  function transferFrom(
+    address src,
+    address dst,
+    uint256 wad
+  ) external returns (bool);
+}
