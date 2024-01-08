64115f80d86f73c1c393b3484c00c73a398d25d5
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2024-01-08 15:02:24 +0100

    feat (test): implement pool create & check test for liq manager + set mainnet as test fork target

--- test/contracts/liquidityManager.test.ts
+++ test/contracts/liquidityManager.test.ts
@@ -8,6 +8,38 @@ export function liquidityManager() {
     before(async function () {});
 
     it("can create pools", async function () {
+      // Create a pool
+      expect(
+        await this.contracts.LiquidityManager.createPool(
+          this.contracts.TetherToken.address, // paymentAsset
+          0, // strategyId
+          0, // protocolShare
+          ...this.protocolConfig.poolMarket,
+          [], // compatiblePools
+        ).then((tx) => tx.wait()),
+      ).to.not.throw;
+
+      // Check pool info
+      const poolInfo = await this.contracts.LiquidityManager.poolInfo(0);
+      expect(poolInfo.paymentAsset.toLowerCase()).to.equal(
+        this.contracts.TetherToken.address,
+      );
+      expect(poolInfo.underlyingAsset.toLowerCase()).to.equal(
+        this.contracts.TetherToken.address,
+      );
+      expect(poolInfo.protocolShare).to.equal(0);
+      expect(poolInfo.strategyId).to.equal(0);
+      expect(poolInfo.formula.uOptimal).to.equal(
+        this.protocolConfig.poolMarket[0],
+      );
+      expect(poolInfo.formula.r0).to.equal(this.protocolConfig.poolMarket[1]);
+      expect(poolInfo.formula.rSlope1).to.equal(
+        this.protocolConfig.poolMarket[2],
+      );
+      expect(poolInfo.formula.rSlope2).to.equal(
+        this.protocolConfig.poolMarket[3],
+      );
+      expect(poolInfo.poolId).to.equal(0);
     });
 
     it("accepts LPs", async function () {});
