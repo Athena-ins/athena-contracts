fc7d402a6b8db44710fb110814759c07490946a1
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2024-02-21 17:12:48 +0100

    chore (helpers): almost equal state with built state + add ts to tx handler

--- test/helpers/chai/almostEqualState.ts
+++ test/helpers/chai/almostEqualState.ts
@@ -3,47 +3,56 @@ import { BigNumber } from "ethers";
 import { LiquidityManager } from "../../../typechain";
 const { expect } = chai;
 
-type PoolInfo = Awaited<ReturnType<LiquidityManager["poolInfo"]>>;
-// poolId: BigNumber;
-// feeRate: BigNumber;
-// formula: {
-//   uOptimal: BigNumber;
-//   r0: BigNumber;
-//   rSlope1: BigNumber;
-//   rSlope2: BigNumber;
-// };
-// slot0: {
-//   tick: number;
-//   secondsPerTick: BigNumber;
-//   coveredCapital: BigNumber;
-//   remainingCovers: BigNumber;
-//   lastUpdateTimestamp: BigNumber;
-//   liquidityIndex: BigNumber;
-// };
-// strategyId: BigNumber;
-// paymentAsset: string;
-// underlyingAsset: string;
-// wrappedAsset: string;
-// isPaused: boolean;
-// overlappedPools: BigNumber[];
-// compensationIds: BigNumber[];
-type PositionInfo = Awaited<ReturnType<LiquidityManager["positionInfo"]>>;
-// supplied: BigNumber;
-// commitWithdrawalTimestamp: BigNumber;
-// rewardIndex: BigNumber;
-// poolIds: BigNumber[];
-// newUserCapital: BigNumber;
-// coverRewards: BigNumber[];
-// strategyRewards: BigNumber;
-type CoverInfo = Awaited<ReturnType<LiquidityManager["coverInfo"]>>;
-// coverId: BigNumber;
-// poolId: BigNumber;
-// coverAmount: BigNumber;
-// start: BigNumber;
-// end: BigNumber;
-// premiumsLeft: BigNumber;
-// dailyCost: BigNumber;
-// premiumRate: BigNumber;
+type PoolInfo =
+  | Awaited<ReturnType<LiquidityManager["poolInfo"]>>
+  | {
+      poolId: BigNumber;
+      feeRate: BigNumber;
+      formula: {
+        uOptimal: BigNumber;
+        r0: BigNumber;
+        rSlope1: BigNumber;
+        rSlope2: BigNumber;
+      };
+      slot0: {
+        tick: number;
+        secondsPerTick: BigNumber;
+        coveredCapital: BigNumber;
+        remainingCovers: BigNumber;
+        lastUpdateTimestamp: BigNumber;
+        liquidityIndex: BigNumber;
+      };
+      strategyId: BigNumber;
+      paymentAsset: string;
+      underlyingAsset: string;
+      wrappedAsset: string;
+      isPaused: boolean;
+      overlappedPools: BigNumber[];
+      compensationIds: BigNumber[];
+    };
+type PositionInfo =
+  | Awaited<ReturnType<LiquidityManager["positionInfo"]>>
+  | {
+      supplied: BigNumber;
+      commitWithdrawalTimestamp: BigNumber;
+      rewardIndex: BigNumber;
+      poolIds: BigNumber[];
+      newUserCapital: BigNumber;
+      coverRewards: BigNumber[];
+      strategyRewards: BigNumber;
+    };
+type CoverInfo =
+  | Awaited<ReturnType<LiquidityManager["coverInfo"]>>
+  | {
+      coverId: BigNumber;
+      poolId: BigNumber;
+      coverAmount: BigNumber;
+      start: BigNumber;
+      end: BigNumber;
+      premiumsLeft: BigNumber;
+      dailyCost: BigNumber;
+      premiumRate: BigNumber;
+    };
 
 declare global {
   export namespace Chai {
@@ -57,10 +66,13 @@ chai.use(function (chai, utils) {
   chai.Assertion.addMethod(
     "almostEqualState",
     function (
-      this: Chai.AssertionStatic,
-      expected: PoolInfo | PositionInfo | CoverInfo,
+      this: Chai.AssertionStatic & {
+        _obj: PoolInfo | PositionInfo | CoverInfo;
+      },
+      input: PoolInfo | PositionInfo | CoverInfo,
     ) {
       const assert = this.assert;
+
       function checkKey(key: any, actualKey: any, expectedKey: any) {
         expect(
           actualKey != undefined,
@@ -108,52 +120,54 @@ chai.use(function (chai, utils) {
         }
       }
 
-      var actual = this._obj as PoolInfo | PositionInfo | CoverInfo;
-
+      const expected = input as any;
+      const actual = this._obj;
       const keys = Object.keys(actual);
 
-      keys.forEach((key: any) => {
-        if (
-          key === "feeRate" ||
-          key === "formula" ||
-          key === "strategyId" ||
-          key === "paymentAsset" ||
-          key === "underlyingAsset" ||
-          key === "wrappedAsset"
-        ) {
-          // skipping consistency check on accessory data
-          return;
-        }
+      for (const key of keys) {
+        // if (
+        //   key === "feeRate" ||
+        //   key === "formula" ||
+        //   key === "strategyId" ||
+        //   key === "paymentAsset" ||
+        //   key === "underlyingAsset" ||
+        //   key === "wrappedAsset"
+        // ) {
+        //   // skipping consistency check on accessory data
+        //   return;
+        // }
 
         if (Array.isArray(actual[key])) {
-          const actualArray = actual[key] as any[];
+          // For arrays we will check every item
+
+          const actualArray = actual[key];
           if (actualArray.length === 0) return;
 
           for (let i = 0; i < actualArray.length; i++) {
-            checkKey(
-              key,
-              (actual[key] as any[])[i],
-              (expected[key] as any[])[i],
-            );
+            checkKey(key, actual[key][i], expected[key][i]);
           }
         } else if (
           typeof actual[key] === "object" &&
           !BigNumber.isBigNumber(actual[key])
         ) {
+          // For objects that are not big numbers we will check the keys
+
           const actualKeys: any[] = Object.keys(actual[key]);
           const expectedKeys: any[] = Object.keys(expected[key]);
 
           for (let i = 0; i < actualKeys.length; i++) {
             checkKey(
               key,
-              (actual[key] as any)[actualKeys[i]],
-              (expected[key] as any)[expectedKeys[i]],
+              actual[key][actualKeys[i]],
+              expected[key][expectedKeys[i]],
             );
           }
         } else {
+          // Else run a simple check
+
           checkKey(key, actual[key], expected[key]);
         }
-      });
+      }
     },
   );
 });
