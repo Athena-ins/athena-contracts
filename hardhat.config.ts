a274cf551b93d1fd8e2e52f410c7d4e5c6b4f142
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2024-02-23 18:26:27 +0100

    chore (hardhat): buggy option sometimes + add try catch in context fn

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -168,7 +168,6 @@ const config: HardhatUserConfig = {
   typechain: {
     outDir: "typechain",
     target: "ethers-v5",
-    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
   },
 
   // ====== Paths ====== //
--- test/context.ts
+++ test/context.ts
@@ -62,74 +62,79 @@ function beforeEachSuite(fn: AsyncFunc) {
 export function baseContext(description: string, hooks: () => void): void {
   describe(description, function () {
     before(async function () {
-      const liqManagerSize = LiquidityManager__factory.bytecode.length / 2;
-      console.log(`\nLiq. Manager size: ${liqManagerSize}/24576\n`);
+      try {
+        const liqManagerSize = LiquidityManager__factory.bytecode.length / 2;
+        console.log(`\nLiq. Manager size: ${liqManagerSize}/24576\n`);
 
-      // Provides signers for testing
-      const nbSpecialAccounts = 5;
-      const signers = await ethers.getSigners();
-      this.signers = {
-        deployer: signers[0] as Signer as Wallet,
-        evidenceGuardian: signers[1] as Signer as Wallet,
-        buybackWallet: signers[2] as Signer as Wallet,
-        treasuryWallet: signers[3] as Signer as Wallet,
-        leverageRiskWallet: signers[4] as Signer as Wallet,
-        //
-        user: signers[nbSpecialAccounts] as Signer as Wallet,
-        user2: signers[nbSpecialAccounts + 1] as Signer as Wallet,
-        user3: signers[nbSpecialAccounts + 2] as Signer as Wallet,
-      };
+        // Provides signers for testing
+        const nbSpecialAccounts = 5;
+        const signers = await ethers.getSigners();
+        this.signers = {
+          deployer: signers[0] as Signer as Wallet,
+          evidenceGuardian: signers[1] as Signer as Wallet,
+          buybackWallet: signers[2] as Signer as Wallet,
+          treasuryWallet: signers[3] as Signer as Wallet,
+          leverageRiskWallet: signers[4] as Signer as Wallet,
+          //
+          user: signers[nbSpecialAccounts] as Signer as Wallet,
+          user2: signers[nbSpecialAccounts + 1] as Signer as Wallet,
+          user3: signers[nbSpecialAccounts + 2] as Signer as Wallet,
+        };
 
-      this.protocolConfig = defaultProtocolConfig;
+        this.protocolConfig = defaultProtocolConfig;
 
-      // Setup protocol for testing & provide interfaces to tests
-      this.contracts = await deployAllContractsAndInitializeProtocol(
-        this.signers.deployer,
-        defaultProtocolConfig,
-        // true,
-      );
+        // Setup protocol for testing & provide interfaces to tests
+        this.contracts = await deployAllContractsAndInitializeProtocol(
+          this.signers.deployer,
+          defaultProtocolConfig,
+          // true,
+        );
 
-      if (
-        this.signers.evidenceGuardian.address !=
-        evidenceGuardianWallet().address
-      )
-        throw Error("Evidence guardian address mismatch");
+        if (
+          this.signers.evidenceGuardian.address !=
+          evidenceGuardianWallet().address
+        )
+          throw Error("Evidence guardian address mismatch");
 
-      // Get WETH for all accounts
-      await Promise.all(
-        Object.values(this.signers).map((signer) =>
-          this.contracts.WethToken.connect(signer)
-            .deposit({
-              value: ethers.utils.parseEther("1000"),
-            })
-            .then((tx) => tx.wait()),
-        ),
-      );
+        // Get WETH for all accounts
+        await Promise.all(
+          Object.values(this.signers).map((signer) =>
+            this.contracts.WethToken.connect(signer)
+              .deposit({
+                value: ethers.utils.parseEther("1000"),
+              })
+              .then((tx) => tx.wait()),
+          ),
+        );
 
-      const logData = {
-        chainId: await entityProviderChainId(this.signers.deployer),
-        deployer: this.signers.deployer.address,
-        evidenceGuardian: this.signers.evidenceGuardian.address,
-        buybackWallet: this.signers.buybackWallet.address,
-        user: this.signers.user.address,
-        user2: this.signers.user2.address,
-        user3: this.signers.user3.address,
-        // protocolConfig: this.protocolConfig,
-        // contracts: Object.keys(this.contracts),
-      };
+        const logData = {
+          chainId: await entityProviderChainId(this.signers.deployer),
+          deployer: this.signers.deployer.address,
+          evidenceGuardian: this.signers.evidenceGuardian.address,
+          buybackWallet: this.signers.buybackWallet.address,
+          user: this.signers.user.address,
+          user2: this.signers.user2.address,
+          user3: this.signers.user3.address,
+          // protocolConfig: this.protocolConfig,
+          // contracts: Object.keys(this.contracts),
+        };
 
-      // Make instance of helpers connected to contracts
-      this.helpers = await makeTestHelpers(
-        this.signers.deployer,
-        this.contracts,
-      );
+        // Make instance of helpers connected to contracts
+        this.helpers = await makeTestHelpers(
+          this.signers.deployer,
+          this.contracts,
+        );
 
-      // Used to restore fork at this point in the test suites
-      this.snapshortId = await makeForkSnapshot();
+        // Used to restore fork at this point in the test suites
+        this.snapshortId = await makeForkSnapshot();
 
-      // console.log(
-      //   `\n=> Test context setup:\n${JSON.stringify(logData, null, 2)}\n`,
-      // );
+        // console.log(
+        //   `\n=> Test context setup:\n${JSON.stringify(logData, null, 2)}\n`,
+        // );
+      } catch (err: any) {
+        console.error(err);
+        throw Error("Test context creation failed");
+      }
     });
 
     // This is run before each child test suite
