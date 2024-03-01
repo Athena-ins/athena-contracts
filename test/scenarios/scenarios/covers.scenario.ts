85daf51b6a9f56567d482784dbb72f6daef67f62
Blackwhale <victor.blackwhale@gmail.com>
2024-03-01 16:48:02 +0100

    chore (scenarios): extend scenario for all existing actions to test calculators

--- test/scenarios/scenarios/covers.scenario.ts
+++ test/scenarios/scenarios/covers.scenario.ts
@@ -43,7 +43,7 @@ export const coverOpenAndUpdate: Scenario = {
           userName: "user0",
           name: "openPosition",
           args: {
-            amount: 10_000,
+            amount: 5000,
             isWrapped: false,
             poolIds: [0],
           },
@@ -80,6 +80,84 @@ export const coverOpenAndUpdate: Scenario = {
             premiumAmount: 20,
           },
           expected: "success",
+        },
+        {
+          userName: "user0",
+          name: "addLiquidity",
+          expected: "success",
+          args: {
+            positionId: 0,
+            isWrapped: false,
+            amount: 1500,
+          },
+        },
+        {
+          userName: "user0",
+          name: "commitRemoveLiquidity",
+          expected: "success",
+          args: {
+            positionId: 0,
+          },
+          timeTravel: {
+            days: 16,
+          },
+        },
+        {
+          userName: "user0",
+          name: "removeLiquidity",
+          expected: "success",
+          args: {
+            positionId: 0,
+            amount: 500,
+            keepWrapped: false,
+          },
+        },
+        {
+          userName: "user0",
+          name: "commitRemoveLiquidity",
+          expected: "success",
+          args: {
+            positionId: 0,
+          },
+        },
+        {
+          userName: "user0",
+          name: "uncommitRemoveLiquidity",
+          expected: "success",
+          args: {
+            positionId: 0,
+          },
+        },
+        {
+          userName: "user1",
+          name: "updateCover",
+          expected: "success",
+          args: {
+            coverId: 0,
+            coverToAdd: 120,
+            coverToRemove: 0,
+            premiumToAdd: 0,
+            premiumToRemove: 10,
+          },
+        },
+        {
+          userName: "user1",
+          name: "initiateClaim",
+          expected: "success",
+          args: {
+            coverId: 0,
+            amountClaimed: 100,
+          },
+          timeTravel: { days: 15 },
+        },
+
+        {
+          userName: "user1",
+          name: "withdrawCompensation",
+          expected: "success",
+          args: {
+            claimId: 0,
+          },
         },
       ],
     },
