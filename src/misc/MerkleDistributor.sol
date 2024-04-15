242cf6a1080b072f16f94b2a0cca62a9e97fd7de
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2024-04-15 13:57:54 +0200

    chore (arch): reorg contract folders

--- src/misc/MerkleDistributor.sol
+++ src/misc/MerkleDistributor.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.20;
 import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 // libraries
-import { PendingRoot } from "./interfaces/IMerkleDistributor.sol";
+import { PendingRoot } from "../interfaces/IMerkleDistributor.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
