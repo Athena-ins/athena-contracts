862f9134d5d0df4f4ac6e068378215a45e04237b
Blackwhale <victor.blackwhale@gmail.com>
2024-04-22 13:22:46 +0200

    chore (scripts) add deploy & verify params

---
+++ scripts/verificationData/addresses.ts
@@ -0,0 +1,35 @@
+import hre from "hardhat";
+import { ProtocolContracts } from "../../test/helpers/deployers";
+
+export type ProtocolContractsAddresses = {
+  [K in keyof ProtocolContracts]: string;
+};
+
+function fromFork() {
+  const forkTarget = process.env.HARDHAT_FORK_TARGET?.toLowerCase();
+
+  if (!forkTarget || forkTarget !== "arbitrum") {
+    throw Error("Missing or erroneous fork target");
+  }
+
+  return forkTarget === "arbitrum" ? "arbitrum" : "";
+}
+
+const networkAddresses: {
+  [key: string]: ProtocolContractsAddresses;
+} = {
+  // arbitrum: {
+  // },
+};
+
+export function getNetworkAddresses() {
+  const networkName = hre.network.name.toUpperCase();
+  const contracts =
+    networkName === "HARDHAT"
+      ? networkAddresses[fromFork()]
+      : networkAddresses[networkName];
+
+  if (!contracts) throw Error(`Missing addresses for network ${networkName}`);
+
+  return contracts;
+}
---
+++ scripts/verificationData/deployParams.ts
@@ -0,0 +1,66 @@
+import hre, { ethers } from "hardhat";
+import { ProtocolConfig } from "../../test/helpers/deployers";
+import {
+  buybackWallet,
+  evidenceGuardianWallet,
+  leverageRiskWallet,
+  toErc20,
+  treasuryWallet,
+} from "../../test/helpers/protocol";
+import { toRay } from "../../test/helpers/utils/poolRayMath";
+
+const utils = ethers.utils;
+
+function fromFork() {
+  const forkTarget = process.env.HARDHAT_FORK_TARGET?.toLowerCase();
+
+  if (!forkTarget || forkTarget !== "arbitrum") {
+    throw Error("Missing or erroneous fork target");
+  }
+
+  return forkTarget === "arbitrum" ? "arbitrum" : "";
+}
+
+const deployParams: {
+  [key: string]: ProtocolConfig;
+} = {
+  arbitrum: {
+    subcourtId: 2,
+    nbOfJurors: 4,
+    arbitrationCollateral: utils.parseEther("0.05"), // in ETH
+    evidenceGuardian: evidenceGuardianWallet(),
+    buybackWallet: buybackWallet(),
+    treasuryWallet: treasuryWallet(),
+    leverageRiskWallet: leverageRiskWallet(),
+    leverageFeePerPool: toRay(1.5, 2), // 1.5% base 100
+    poolFormula: {
+      feeRate: toRay(0.1), // 10%
+      uOptimal: toRay(75),
+      r0: toRay(1),
+      rSlope1: toRay(5),
+      rSlope2: toRay(10),
+    },
+    yieldBonuses: [
+      { atenAmount: toErc20(0), yieldBonus: toRay(250, 4) },
+      { atenAmount: toErc20(1_000), yieldBonus: toRay(200, 4) },
+      { atenAmount: toErc20(100_000), yieldBonus: toRay(150, 4) },
+      { atenAmount: toErc20(1_000_000), yieldBonus: toRay(50, 4) },
+    ],
+    withdrawDelay: 14 * 24 * 60 * 60, // 14 days
+    maxLeverage: 12, // max pools per position
+    payoutDeductibleRate: toRay(0.1), // 10%
+    performanceFee: toRay(0.5), // 50%
+  },
+};
+
+export function getDeployParams() {
+  const networkName = hre.network.name.toUpperCase();
+  const contracts =
+    networkName === "HARDHAT"
+      ? deployParams[fromFork()]
+      : deployParams[networkName];
+
+  if (!contracts) throw Error(`Missing addresses for network ${networkName}`);
+
+  return contracts;
+}
