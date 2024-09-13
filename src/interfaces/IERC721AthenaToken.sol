e18af9f64a6532e4c56b457331733ad7127181ff
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2024-09-13 18:59:17 +0200

    chore (interfaces): rename

--- src/interfaces/IERC721AthenaToken.sol
+++ src/interfaces/IERC721AthenaToken.sol
@@ -4,6 +4,23 @@ pragma solidity ^0.8.25;
 /// @dev This interface is only meant for documentation generation purposes
 interface IERC721AthenaToken {
   /**
+   * @dev Returns the token collection name.
+   */
+  function name() external view returns (string memory);
+
+  /**
+   * @dev Returns the token collection symbol.
+   */
+  function symbol() external view returns (string memory);
+
+  /**
+   * @dev Returns the Uniform Resource Identifier (URI) for `tokenId` token.
+   */
+  function tokenURI(
+    uint256 tokenId
+  ) external view returns (string memory);
+
+  /**
    * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
    */
   event Transfer(
